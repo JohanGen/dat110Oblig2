63421d78b0df6d879fe5170df97f53588156d440
669773 <111857758+669773@users.noreply.github.com>
2024-03-07 14:40:25 +0100

    ferdig SubscribeMsg.java

--- src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/main/java/no/hvl/dat110/messages/SubscribeMsg.java
@@ -2,13 +2,27 @@ package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
 
-	// message sent from client to subscribe on a topic 
+	// message sent from client to subscribe on a topic
+	private String topic;
 
     public SubscribeMsg(String user, String topic) {
-
+    	super(MessageType.SUBSCRIBE, user);
+    	this.topic = topic;
+    }
+    
+    public void setTopic(String topic) {
+    	this.topic = topic;
+    }
+    
+    public String getTopic() {
+    	return topic;
+    }
+    
+    public String toString() {
+    	return "SubscribeMsg [type = " + getType() + " , user= " + getUser() + " , topic= " + topic; 
     }
 
-	// TODO:
+	
 	// Implement object variables - a topic is required
 
 	// Complete the constructor, get/set-methods, and toString method
