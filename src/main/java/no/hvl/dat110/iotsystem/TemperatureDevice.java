b2344c231b04a0f8df8eb6a459771777a4a98b1f
669773 <111857758+669773@users.noreply.github.com>
2024-03-07 15:08:16 +0100

    ferdig TemperatureDevice.java

--- src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -1,30 +1,44 @@
 package no.hvl.dat110.iotsystem;
 
 import no.hvl.dat110.client.Client;
-import no.hvl.dat110.common.TODO;
 
 public class TemperatureDevice {
 
-	private static final int COUNT = 10;
+    private static final int COUNT = 10;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// simulated / virtual temperature sensor
-		TemperatureSensor sn = new TemperatureSensor();
+        // simulated / virtual temperature sensor
+        TemperatureSensor sn = new TemperatureSensor();
 
-		// TODO - start
+        // create a client object and use it to
+        // - connect to the broker - user "sensor" as the user name
+        // - publish the temperature(s)
+        // - disconnect from the broker
 
-		// create a client object and use it to
+        // Creating a client object
+        Client client = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
 
-		// - connect to the broker - user "sensor" as the user name
-		// - publish the temperature(s)
-		// - disconnect from the broker
+        try {
+            // Connect to the broker
+            client.connect();
 
-		// TODO - end
+            // Publish the temperature(s)
+            for (int i = 0; i < COUNT; i++) {
+                // Simulate getting temperature reading
+                float temperature = sn.read();
+                // Publish the temperature reading
+                client.publish(Common.TEMPTOPIC, String.valueOf(temperature));
+                // Sleep for a while before publishing the next reading
+                Thread.sleep(1000);
+            }
 
-		System.out.println("Temperature device stopping ... ");
+            // Disconnect from the broker
+            client.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		throw new UnsupportedOperationException(TODO.method());
-
-	}
+        System.out.println("Temperature device stopping ... ");
+    }
 }
